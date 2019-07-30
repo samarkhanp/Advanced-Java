689e7a4598dbb64e44c3eaa273e1eb8d93cb1d15
Samar Khan <36196934+samarkhanp@users.noreply.github.com>
2019-07-30 18:49:15 +0530

    Create Key.java

---
+++ AWT Key and Image/Key.java
@@ -0,0 +1,64 @@
+/*
+<applet code="Key" width=300 height=400>
+</applet>
+*/
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+import java.awt.Graphics;
+
+
+public class Key extends Applet
+implements KeyListener
+{
+	int X=20,Y=30,x=200,y=200;
+	String msg="KeyEvents--->";
+Image a;
+	public void init()
+	{
+		addKeyListener(this);
+		requestFocus();
+		setBackground(Color.green);
+		setForeground(Color.blue);
+
+		a=getImage(getDocumentBase(),"Desert.jpg");
+	}
+	public void keyPressed(KeyEvent k)
+	{
+		showStatus("KeyDown");
+		int key=k.getKeyCode();
+		switch(key) 
+		{
+			case KeyEvent.VK_UP:
+				{showStatus("Move to Up");
+					y=y-25;
+				break;
+				}
+			case KeyEvent.VK_DOWN:
+				showStatus("Move to Down"); y=y+25;
+				break;
+			case KeyEvent.VK_LEFT:
+				showStatus("Move to Left"); x=x-25;
+				break;
+			case KeyEvent.VK_RIGHT:
+				showStatus("Move to Right"); x=x+25;
+				break;
+
+		}
+		repaint();
+	}
+	public void keyReleased(KeyEvent k)
+	{
+		showStatus("Key Up");
+	}
+	public void keyTyped(KeyEvent k)
+	{
+		msg+=k.getKeyChar();
+		repaint();
+	}
+	public void paint(Graphics g)
+	{
+		g.drawString(msg,X,Y);
+		g.drawImage(a,x,y,this);
+	}
+}
